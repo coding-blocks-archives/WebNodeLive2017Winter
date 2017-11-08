9e230ea60c460c4fe7b029a35e0cdd7e74ff534b
Arnav Gupta <arnav@codingblocks.com>
2017-11-08 20:26:38 +0530

    update assignment
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- Webinar11/routers-example/routes/students.js
+++ Webinar11/routers-example/routes/students.js
@@ -8,6 +8,14 @@ let students = [
 ]
 
 route.get('/', (req, res) => res.send(students))
+route.post('/', (req, res) => {
+    students.push({
+        name: req.body.name,
+        college: req.body.college,
+        year: req.body.year
+    })
+    res.send(students)
+})
 route.get('/:id', (req, res) => res.send(students[req.params.id]))
 
 module.exports = route
--- Webinar11/routers-example/server.js
+++ Webinar11/routers-example/server.js
@@ -1,7 +1,11 @@
-const srv = require('express')()
+const express = require('express')
+const srv = express()
 
 const teacherRoute = require('./routes/teachers')
 const studentRoute = require('./routes/students')
+
+srv.use(express.json())
+srv.use(express.urlencoded({extended: true}))
 
 srv.use('/students', studentRoute)
 srv.use('/teachers', teacherRoute)
