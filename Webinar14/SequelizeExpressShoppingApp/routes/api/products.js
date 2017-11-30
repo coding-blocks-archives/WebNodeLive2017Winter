69d7951a15b7dd2cd4714d7294283ad457cfd5d5
Arnav Gupta <arnav@codingblocks.com>
2017-11-30 20:38:59 +0530

    basic backend is done
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- Webinar14/SequelizeExpressShoppingApp/routes/api/products.js
+++ Webinar14/SequelizeExpressShoppingApp/routes/api/products.js
@@ -3,10 +3,36 @@ const route = require('express').Router();
 
 route.get('/', (req, res) => {
     // Get all products
+    Product.findAll()
+        .then((products) => {
+            res.status(200).send(products)
+        })
+        .catch((err) => {
+            res.status(500).send({
+                error: "Could not retrieve products"
+            })
+        })
 })
 
 route.post('/', (req, res) => {
+    // Validate the values
+    if (isNaN(req.body.price)) {
+        return res.status(403).send({
+            error: "Price is not valid number"
+        })
+    }
     // Add a new product
+    Product.create({
+        name: req.body.name,
+        manufacturer: req.body.manufacturer,
+        price: parseFloat(req.body.price)
+    }).then((product) => {
+        res.status(201).send(product)
+    }).catch((error) => {
+        res.status(501).send({
+            error: "Error adding product"
+        })
+    })
 })
 
 exports = module.exports = route
